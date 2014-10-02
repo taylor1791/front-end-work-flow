8b4bb82e274f749ece8bae5422ab85abd9486668
Taylor1791 <Taylor@Everding.email>
2014-10-01 23:29:14 -0500

    Added external json validations

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,34 @@
 'use strict';
 
 var
-  gulp       = require( 'gulp' ),
-  plugins    = require( 'gulp-load-plugins' )(),
-  srcFiles      = {
+  gulp      = require( 'gulp' ),
+  plugins   = require( 'gulp-load-plugins' )(),
+
+  // Keeps track of all the different files and their type for linting.
+  fileTypes = {
     json: [ __dirname + '/package.json' ]
   },
-  extensions = { };
+  // This
+  addFiles = {
+    json: []
+  };
 
-exports = module.exports = extensions;
+// Expose the addFiles object so that other gulpfiles can add to the linting
+// tasks. This allows this repository to be a "sub-module" of an existing
+// repository.
+exports = module.exports = exts;
 
-// Generate all the linting tasks
-Object.keys( srcFiles ).forEach( function( fileType ) {
-  var files = srcFiles[ fileType ];
+//
+// Linting is the process of identifing usage of unidomatic features of
+// language. This is a tool to prevent bugs by warning the programmer of the
+// useage of "bad" features.
+
+// Generates all the types of linters.
+Object.keys( fileTypes ).forEach( function( fileType ) {
 
   gulp.task( 'lint-' + fileType, function() {
+    var files = srcFiles[ fileType ].concat( addFiles.json );
+
     gulp.src( files )
       .pipe( plugins.jsonlint() )
       .pipe( plugins.jsonlint.reporter() );
