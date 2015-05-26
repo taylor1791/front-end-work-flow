7edbb82f900d5e5bfcccc98c280df6e1b217f59b
Taylor1791 <Taylor@Everding.email>
2015-05-25 20:20:09 -0500

    add disabled specs for jshint

---
+++ spec/js-hint.js
@@ -0,0 +1,8 @@
+describe( 'js-hint task', function() {
+
+  xit( 'uses few jshintrc as the root' );
+  xit( 'uses cwd jshintrc as 1st override' );
+  xit( 'uses few.jshintrc as a 2nd override' );
+  xit( 'few.jshint as a 3rd override' );
+
+} );
